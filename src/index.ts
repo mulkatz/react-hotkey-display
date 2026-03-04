f98e092de58e3ecfa67bcc50976878bc84898f85
Franz <franz.benthin.dev@gmail.com>
2026-03-04 07:23:13 +0100

    feat: add public API entry point

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Kbd } from "./kbd.js";
+export { Hotkey } from "./hotkey.js";
+export { detectPlatform } from "./detect-os.js";
+export { formatKey, parseCombo, formatCombo, getAriaLabel, getComboAriaLabel } from "./format.js";
+export type { Platform, KbdVariant, KbdProps, HotkeyProps } from "./types.js";
