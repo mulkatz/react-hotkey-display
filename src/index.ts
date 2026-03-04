ac3da9958aa1c871cb244a28dda75c77ff8667c9
Franz <franz.benthin.dev@gmail.com>
2026-03-04 07:44:20 +0100

    refactor: export usePlatform and resetPlatformCache, relax biome a11y rule
    Export the new usePlatform hook and resetPlatformCache utility.
    Disable useSemanticElements biome rule since role="group" on span
    is valid ARIA for inline key grouping.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Kbd } from "./kbd.js";
 export { Hotkey } from "./hotkey.js";
-export { detectPlatform } from "./detect-os.js";
+export { detectPlatform, resetPlatformCache } from "./detect-os.js";
+export { usePlatform } from "./use-platform.js";
 export { formatKey, parseCombo, formatCombo, getAriaLabel, getComboAriaLabel } from "./format.js";
 export type { Platform, KbdVariant, KbdProps, HotkeyProps } from "./types.js";
